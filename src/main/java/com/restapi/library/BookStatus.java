4734c881a6840ce419cb556ea8d2fed4734e1721
gawelx <gawelx@gmail.com>
2019-08-26 19:25:57 +0200

    BookStatus extended to provide more human-friendly data

--- src/main/java/com/restapi/library/BookStatus.java
+++ src/main/java/com/restapi/library/BookStatus.java
@@ -1,8 +1,33 @@
 package com.restapi.library;
 
 public enum BookStatus {
-    AVAILABLE,
-    BORROWED,
-    DAMAGED,
-    LOST
+    AVAILABLE("available"),
+    BORROWED("borrowed"),
+    DAMAGED("damaged"),
+    LOST("lost");
+
+    private String status;
+
+    private BookStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
+
+    static BookStatus getInstance(String status) {
+        switch (status) {
+            case "available":
+                return AVAILABLE;
+            case "borrowed":
+                return BORROWED;
+            case "damaged":
+                return DAMAGED;
+            case "lost":
+                return LOST;
+        }
+        throw new IllegalStateException("Unknown book status.");
+    }
 }
