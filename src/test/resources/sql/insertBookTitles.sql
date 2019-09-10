165731b81e913bc89abe99b204b939143f6dfbbe
gawelx <gawelx@gmail.com>
2019-09-10 12:27:31 +0200

    Preparations for unit test
    *) application.properties file with H2 in-memory database config has been created for test profile only;
    *) hamcrest and hamcrest-optional libraries has been added to build.gradle file to simplify Optionals and Collections tests;
    *) sql scripts with sample data for testing has been created;

---
+++ src/test/resources/sql/insertBookTitles.sql
@@ -0,0 +1,10 @@
+INSERT INTO booktitle (id, title)
+VALUES (11, 'Gone with the Wind'),
+       (12, 'The Witcher'),
+       (13, 'Harry Potter and the Goblet of Fire');
+
+INSERT INTO booktitle_author (bookTitleId, authorId)
+VALUES (11, 1),
+       (12, 1),
+       (12, 4),
+       (13, 2);
